b12d26ef6bf6d95d18d9c2f203d5e92e864991c7
trutzzi <trutzzi@yahoo.ro>
2023-01-13 12:32:38 +0200

    remove format deny

--- src/components/FileImport.tsx
+++ src/components/FileImport.tsx
@@ -9,7 +9,7 @@ export default function FileImport({ onChange }: FileImportProps) {
     return (
         <Button variant='text' color='inherit' component="label">
             Select Report
-            <input hidden accept="xls/*" multiple type="file" onChange={onChange} />
+            <input hidden multiple type="file" onChange={onChange} />
         </Button>
     );
 }
